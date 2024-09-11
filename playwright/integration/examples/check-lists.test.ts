1462221a9f309c1bc68c9561909fe5a70e13e5c1
Ty Mick <ty.mick@logos.com>
2024-09-11 15:12:07 -0700

    Try writing a Playwright test

--- playwright/integration/examples/check-lists.test.ts
+++ playwright/integration/examples/check-lists.test.ts
@@ -33,4 +33,27 @@ test.describe('Check-lists example', () => {
 
     await expect(page.locator('p[data-slate-node="element"]')).toHaveCount(2)
   })
+
+  test.only('dragging selection into checkbox', async ({ page }) => {
+    const slateNodeElement = '[data-slate-node="element"]'
+    const slateNodeText = '[data-slate-node="text"]'
+
+    const paragraph = page.locator(slateNodeElement).nth(0)
+    const initialParagraphContent = await paragraph.textContent()
+    const checklistItem = page.locator(slateNodeElement).nth(1)
+    await expect.poll(() => page.evaluate(() => document.getSelection()!.anchorOffset)).toBe(0)
+    await checklistItem.hover({ position: { x: 65, y: 5 } })
+    await page.mouse.down()
+    await expect.poll(() => page.evaluate(() => document.getSelection()!.anchorOffset)).toBe(5)
+    await paragraph.hover({ position: { x: 20, y: 40 }})
+    await expect.poll(() => page.evaluate(() => document.getSelection()!.toString()))
+      .toMatch(/^haviors.+\n+Slide$/)
+    await checklistItem.hover({ position: { x: 5, y: 5 } })
+    await page.mouse.up()
+    await expect.poll(() => page.evaluate(() => document.getSelection()!.toString())).toBe('Slide')
+    await page.keyboard.press('Backspace')
+
+    await expect(paragraph).toHaveText(initialParagraphContent!)
+    await expect(checklistItem).toHaveText(' to the left.')
+  })
 })
